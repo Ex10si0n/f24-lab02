b3118fc76630940f348eb4da761a7c8c82702f63
ex10si0n <yan.ex10si0n@icloud.com>
2024-09-06 11:25:00 -0400

    lab2 commit: [TS code] defined draw method by calling shape.computeArea()

--- typescript/src/renderer.ts
+++ typescript/src/renderer.ts
@@ -1,5 +1,6 @@
 function newRenderer(shape: Shape) {
     return {
+        // defined draw method by calling shape.computeArea()
         draw() {
             const area: number = shape.computeArea()
             console.log("Shape drawn\n" + "Its area is " + area)
